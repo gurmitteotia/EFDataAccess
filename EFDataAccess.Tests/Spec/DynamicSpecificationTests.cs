11352a9fd2fb4bda2661af382998aebbc6a6d8ee
gurmitteotia <gurmit.teotia@gmail.com>
2015-10-11 17:39:11 +0100

    Updated tests

--- EFDataAccess.Tests/Spec/DynamicSpecificationTests.cs
+++ EFDataAccess.Tests/Spec/DynamicSpecificationTests.cs
@@ -7,211 +7,92 @@ namespace EFDataAccess.Tests.Spec
     [TestFixture]
     public class DynamicSpecificationTests
     {
-        [Test]
-        public void Specification_with_equal_to_operation_type_is_satisfied_when_candiate_property_is_equal_to_dynamic_value()
+        private TestClass _candidateObject;
+
+        [SetUp]
+        public void Setup()
         {
-            var candidateObject = new TestClass() {Salary = 5000};
-            var specification = new DynamicSpecification<TestClass>("Salary",OperationType.EqualTo, 5000);
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsTrue(satisfied);
+            _candidateObject = new TestClass() { Salary = 5000 };
         }
 
         [Test]
-        public void Specification_with_equal_to_operation_type_is_not_satisfied_when_candiate_property_is_not_equal_to_dynamic_value()
+        public void EqualTo_test()
         {
-            var candidateObject = new TestClass() { Salary = 5000 };
-            var specification = new DynamicSpecification<TestClass>("Salary", OperationType.EqualTo, 4000);
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsFalse(satisfied);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.EqualTo, 5000).IsSatisfiedBy(_candidateObject), Is.True);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.EqualTo, 4000).IsSatisfiedBy(_candidateObject), Is.False);
         }
 
         [Test]
-        public void Specification_with_not_equal_to_operation_type_is_satisfied_when_candiate_property_is_not_equal_to_dynamic_value()
+        public void NotEqualTo_test()
         {
-            var candidateObject = new TestClass() { Salary = 5000 };
-            var specification = new DynamicSpecification<TestClass>("Salary", OperationType.NotEqualTo, 4000);
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsTrue(satisfied);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.NotEqualTo, 4000).IsSatisfiedBy(_candidateObject), Is.True);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.NotEqualTo, 5000).IsSatisfiedBy(_candidateObject), Is.False);
         }
 
         [Test]
-        public void Specification_with_not_equal_to_operation_type_is_not_satisfied_when_candiate_property_is_equal_to_dynamic_value()
+        public void GreaterThan_test()
         {
-            var candidateObject = new TestClass() { Salary = 5000 };
-            var specification = new DynamicSpecification<TestClass>("Salary", OperationType.NotEqualTo, 5000);
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsFalse(satisfied);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.GreaterThan, 4000).IsSatisfiedBy(_candidateObject), Is.True);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.GreaterThan, 6000).IsSatisfiedBy(_candidateObject), Is.False);
         }
 
         [Test]
-        public void Specification_with_greater_then_operation_type_is_satisfied_when_candiate_property_is_greater_than_dynamic_value()
+        public void LessThan_test()
         {
-            var candidateObject = new TestClass() { Salary = 5000 };
-            var specification = new DynamicSpecification<TestClass>("Salary", OperationType.GreaterThan, 4000);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.LessThan, 6000).IsSatisfiedBy(_candidateObject), Is.True);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.LessThan, 4000).IsSatisfiedBy(_candidateObject), Is.False);
+        }
 
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
 
-            Assert.IsTrue(satisfied);
+        [Test]
+        public void GreaterThanEqualTo_test()
+        {
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.GreaterThanEqualTo, 5000).IsSatisfiedBy(_candidateObject), Is.True);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.GreaterThanEqualTo, 4000).IsSatisfiedBy(_candidateObject), Is.True);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.GreaterThanEqualTo, 6000).IsSatisfiedBy(_candidateObject), Is.False);
         }
 
         [Test]
-        public void Specification_with_greater_then_operation_type_is_not_satisfied_when_candiate_property_is_less_than_dynamic_value()
+        public void LessThanEqualTo_test()
         {
-            var candidateObject = new TestClass() { Salary = 4000 };
-            var specification = new DynamicSpecification<TestClass>("Salary", OperationType.GreaterThan, 5000);
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsFalse(satisfied);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.LessThanEqualTo, 6000).IsSatisfiedBy(_candidateObject), Is.True);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.LessThanEqualTo, 5000).IsSatisfiedBy(_candidateObject), Is.True);
+            Assert.That(new DynamicSpecification<TestClass>("Salary", OperationType.LessThanEqualTo, 3000).IsSatisfiedBy(_candidateObject), Is.False);
         }
 
         [Test]
-        public void Specification_with_greater_then_equal_to_operation_type_is_satisfied_when_candiate_property_is_greater_than_or_equal_to_dynamic_value()
-        {
-            var candidateObject = new TestClass() { Salary = 4000 };
-            var specification = new DynamicSpecification<TestClass>("Salary", OperationType.GreaterThanEqualTo, 4000);
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsTrue(satisfied);
-        }
-
-        [Test]
-        public void Specification_with_greater_then_equal_to_operation_type_is_not_satisfied_when_candiate_property_is_less_than_dynamic_value()
-        {
-            var candidateObject = new TestClass() { Salary = 4000 };
-            var specification = new DynamicSpecification<TestClass>("Salary", OperationType.GreaterThanEqualTo, 5000);
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsFalse(satisfied);
-        }
-
-        [Test]
-        public void Specification_with_less_than_operation_type_is_satisfied_when_candiate_property_is_less_than_dynamic_value()
-        {
-            var candidateObject = new TestClass() { Salary = 4000 };
-            var specification = new DynamicSpecification<TestClass>("Salary", OperationType.LessThan, 5000);
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsTrue(satisfied);
-        }
-
-        [Test]
-        public void Specification_with_less_than_operation_type_is_not_satisfied_when_candiate_property_is_greater_than_dynamic_value()
-        {
-            var candidateObject = new TestClass() { Salary = 6000 };
-            var specification = new DynamicSpecification<TestClass>("Salary", OperationType.LessThan, 5000);
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsFalse(satisfied);
-        }
-
-        [Test]
-        public void Specification_with_less_than_equal_to_operation_type_is_satisfied_when_candiate_property_is_less_than_or_equal_to_dynamic_value()
-        {
-            var candidateObject = new TestClass() { Salary = 4000 };
-            var specification = new DynamicSpecification<TestClass>("Salary", OperationType.LessThanEqualTo, 4000);
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsTrue(satisfied);
-        }
-
-        [Test]
-        public void Specification_with_less_than_equal_to_operation_type_is_not_satisfied_when_candiate_property_is_greater_than_dynamic_value()
-        {
-            var candidateObject = new TestClass() { Salary = 6000 };
-            var specification = new DynamicSpecification<TestClass>("Salary", OperationType.LessThanEqualTo, 5000);
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsFalse(satisfied);
-        }
-
-        [Test]
-        public void Specification_with_contains_operation_type_is_satisfied_when_candiate_property_has_dynamic_value()
+        public void Contains_test()
         {
             var candidateObject = new TestClass() { Department = "Sales & Marketing" };
-            var specification = new DynamicSpecification<TestClass>("Department", OperationType.Contains, "Market");
 
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsTrue(satisfied);
+            Assert.That(new DynamicSpecification<TestClass>("Department", OperationType.Contains, "Market").IsSatisfiedBy(candidateObject),Is.True);
+            Assert.That(new DynamicSpecification<TestClass>("Department", OperationType.Contains, "HR").IsSatisfiedBy(candidateObject), Is.False);
         }
 
         [Test]
-        public void Specification_with_contains_operation_type_is_not_satisfied_when_candiate_property_does_not_has_dynamic_value()
+        public void StartWith_test()
         {
             var candidateObject = new TestClass() { Department = "Sales & Marketing" };
-            var specification = new DynamicSpecification<TestClass>("Department", OperationType.Contains, "HR");
 
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.False(satisfied);
+            Assert.That(new DynamicSpecification<TestClass>("Department", OperationType.StartsWith, "Sales").IsSatisfiedBy(candidateObject), Is.True);
+            Assert.That(new DynamicSpecification<TestClass>("Department", OperationType.StartsWith, "HR").IsSatisfiedBy(candidateObject), Is.False);
         }
 
         [Test]
-        public void Specification_with_start_with_operation_type_is_satisfied_when_candiate_property_begins_with_dynamic_value()
+        public void EndsWith_test()
         {
             var candidateObject = new TestClass() { Department = "Sales & Marketing" };
-            var specification = new DynamicSpecification<TestClass>("Department", OperationType.StartsWith, "Sales");
 
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsTrue(satisfied);
-        }
-
-        [Test]
-        public void Specification_with_start_with_operation_type_is_not_satisfied_when_candiate_property_does_not_begin_with_dynamic_value()
-        {
-            var candidateObject = new TestClass() { Department = "Sales & Marketing" };
-            var specification = new DynamicSpecification<TestClass>("Department", OperationType.Contains, "HR");
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.False(satisfied);
-        }
-
-        [Test]
-        public void Specification_with_ends_with_operation_type_is_satisfied_when_candiate_property_end_with_dynamic_value()
-        {
-            var candidateObject = new TestClass() { Department = "Sales & Marketing" };
-            var specification = new DynamicSpecification<TestClass>("Department", OperationType.EndsWith, "Marketing");
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.IsTrue(satisfied);
-        }
-
-        [Test]
-        public void Specification_with_ends_with_operation_type_is_not_satisfied_when_candiate_property_does_not_end_with_dynamic_value()
-        {
-            var candidateObject = new TestClass() { Department = "Sales & Marketing" };
-            var specification = new DynamicSpecification<TestClass>("Department", OperationType.EndsWith, "HR");
-
-            var satisfied = specification.IsSatisfiedBy(candidateObject);
-
-            Assert.False(satisfied);
+            Assert.That(new DynamicSpecification<TestClass>("Department", OperationType.EndsWith, "Marketing").IsSatisfiedBy(candidateObject), Is.True);
+            Assert.That(new DynamicSpecification<TestClass>("Department", OperationType.EndsWith, "HR").IsSatisfiedBy(candidateObject), Is.False);
         }
 
         [Test]
         public void Throws_exception_when_dynamic_property_is_not_found_in_field_and_properties_of_candidate_object()
         {
-            var candidateObject = new TestClass();
-            var specification = new DynamicSpecification<TestClass>("Name", OperationType.EndsWith, "Smith");
+            var specification = new DynamicSpecification<TestClass>("Age", OperationType.GreaterThan, 30);
 
-            TestDelegate matchingOfSpecification = ()=> specification.IsSatisfiedBy(candidateObject);
+            TestDelegate matchingOfSpecification = ()=> specification.IsSatisfiedBy(_candidateObject);
 
             Assert.That(matchingOfSpecification,Throws.TypeOf<ArgumentException>());
 
